e7c689441dbcb229a85039c98af3ba9df80f1e5b
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-04-03 17:19:11 +0200

    First changes and components

--- AccountingOfficeManager/src/app/app.module.ts
+++ AccountingOfficeManager/src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HomeComponent } from './components/home/home.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './components/login/login.component';
+
+const components = [
+  AppComponent,
+]
+
+const material = [
+  MatSidenavModule,
+  MatDividerModule,
+  MatIconModule,
+  MatButtonModule
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    components,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    material
   ],
   providers: [],
   bootstrap: [AppComponent]
