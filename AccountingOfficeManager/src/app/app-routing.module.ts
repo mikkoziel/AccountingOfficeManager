e5e6cb4ef7fe9c8839a2cb161fa6601d6dd8a1ed
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-05-17 19:38:16 +0200

    Changes

--- AccountingOfficeManager/src/app/app-routing.module.ts
+++ AccountingOfficeManager/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent},
 ]
 
 @NgModule({
