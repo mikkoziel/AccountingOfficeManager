b95cf17582de8aebef326147d7c57f200c5803ee
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-07-18 00:28:24 +0200

    Firt try with ClientmanagemenetComponent

--- AccountingOfficeManager/src/app/components/client-management/client-management.component.ts
+++ AccountingOfficeManager/src/app/components/client-management/client-management.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ClientCompany } from 'src/app/entity/clientCompany';
+import { Client } from 'src/app/entity/client';
+import { User } from 'src/app/entity/user';
+import { ClientService } from 'src/app/services/client.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -8,14 +10,20 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./client-management.component.css']
 })
 export class ClientManagementComponent implements OnInit {
-  clients: Array<ClientCompany>
+  currentuser: User;
+  clients: Array<Client>;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private clientService: ClientService
   ) { }
 
   ngOnInit(): void {
-    // this.clients = this.userService.getClients()
+    this.currentuser = this.userService.getCurrentUser();
+    this.clientService.getClientsForEmployee(this.currentuser.id).subscribe(res=>{
+      console.log(res)
+      this.clients = res;
+    })
   }
 
 }
