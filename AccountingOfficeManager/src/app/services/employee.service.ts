467a3a44867cb542e132e98f499ba9df52bd2e59
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-07-17 23:25:23 +0200

    Added EmployeeService

---
+++ AccountingOfficeManager/src/app/services/employee.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { WorkLog } from '../entity/worklog';
+import { ServerService } from './server.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(
+    private server: ServerService,
+    ) { }
+
+  getWorkLogs(id){
+    return this.server.request('GET', '/user/' + id)
+      .pipe(
+        // tap((res:Response) => console.log(res)),
+        map((res: any) => {
+          var worklogs = new Array<WorkLog>();
+          res.worklog.forEach(x=>
+            worklogs.push(this.parseWorkLog(x))
+          );
+          return worklogs;
+        })
+      );
+  }
+
+  saveWorklog(date, duration, user_id){
+    var data = {
+      "employee":{
+        "user_id": user_id,
+      },
+      "date": date,
+      "duration": duration,
+    }
+    console.log(data)
+    return this.server.request('POST', '/work-log/', data)
+  }
+
+  parseWorkLog(data): WorkLog{
+    return <WorkLog> {
+      worklog_id: data['worklog_id'],
+      date: data['date'],
+      duration: this.convertWorklogDuration(data['duration'])
+    }
+  }
+
+  convertWorklogDuration(dur){
+    return Math.round((dur /(60 * 60) + Number.EPSILON) * 100) / 100;
+  }
+}
