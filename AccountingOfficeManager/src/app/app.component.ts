1d379edb6d9030facdbf9ca7705d56a9ee333573
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-07-31 23:36:33 +0200

    Added currentUser to main component

--- AccountingOfficeManager/src/app/app.component.ts
+++ AccountingOfficeManager/src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from './entity/user';
 import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Accounting Office Manager';
   isMenuCollapsed = false;
+  currentUser: User;
   
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
+    ) {}
+
+    
+  ngOnInit(): void {
+    this.currentUser = this.userService.getCurrentUser();
+  }
 
   onLogout() {
     this.authService.logout();
