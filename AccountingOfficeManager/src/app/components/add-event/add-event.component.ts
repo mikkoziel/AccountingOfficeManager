c3410a8be7c53820ad09d9370c6982feba6b37cb
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-08-18 01:16:35 +0200

    Added form handling

--- AccountingOfficeManager/src/app/components/add-event/add-event.component.ts
+++ AccountingOfficeManager/src/app/components/add-event/add-event.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from 'src/app/entity/user';
+import { CalendarService } from 'src/app/services/calendar.service';
+import { UserService } from 'src/app/services/user.service';
 import { CalendarComponent } from '../calendar/calendar.component';
 
 @Component({
@@ -8,13 +12,37 @@ import { CalendarComponent } from '../calendar/calendar.component';
   styleUrls: ['./add-event.component.css']
 })
 export class AddEventComponent implements OnInit {
+  form: FormGroup;
+  currentUser: User;
 
   constructor(
     public dialogRef: MatDialogRef<AddEventComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: CalendarComponent
+    @Inject(MAT_DIALOG_DATA) public data: CalendarComponent,
+    private fb: FormBuilder, 
+    private calendarService: CalendarService,
+    private userService: UserService,
   ) { }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user =>{
+      this.currentUser = user;
+    })
+    this.form = this.fb.group({
+      start_date: ['', Validators.required],
+      end_date: ['', Validators.required],
+      title: ['', Validators.required],
+      all_day: ['', Validators.required]
+    });
+  }
+
+  async onSubmit(){
+    await this.calendarService.saveCalendarEvent(
+      this.currentUser.id,
+      this.form.value["start_date"],
+      this.form.value["end_date"],
+      this.form.value["title"],
+      this.form.value["all_day"],
+    );
   }
 
   onNoClick(): void {
