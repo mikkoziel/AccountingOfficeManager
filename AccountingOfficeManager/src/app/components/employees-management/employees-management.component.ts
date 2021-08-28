037a0040457e16b648cbf34f197b46312a49c9ed
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-08-28 17:36:06 +0200

    Added button and dialog to  EmployeesManagement

--- AccountingOfficeManager/src/app/components/employees-management/employees-management.component.ts
+++ AccountingOfficeManager/src/app/components/employees-management/employees-management.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Employee } from 'src/app/entity/employee';
 import { User } from 'src/app/entity/user';
 import { EmployeeService } from 'src/app/services/employee.service';
 import { UserService } from 'src/app/services/user.service';
+import { AddEmployeeComponent } from '../add-employee/add-employee.component';
 
 @Component({
   selector: 'app-employees-management',
@@ -16,7 +18,8 @@ export class EmployeesManagementComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private eService: EmployeeService
+    private eService: EmployeeService,
+    private dialog: MatDialog,
     ) { }
 
   ngOnInit(): void {
@@ -27,6 +30,18 @@ export class EmployeesManagementComponent implements OnInit {
         this.employees = res;
       })
     })
+  }
+
+  addNewEmployee(){
+    let dialogRef = this.dialog.open(AddEmployeeComponent, {
+      height: '400px',
+      width: '600px',
+      // data: {'company_id': this.company_id}
+    });
+    
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
