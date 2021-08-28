83eb5408f4c8e266e541abcff2510586fc81d8eb
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-08-28 17:36:33 +0200

    AddEmployee works

--- AccountingOfficeManager/src/app/components/add-employee/add-employee.component.ts
+++ AccountingOfficeManager/src/app/components/add-employee/add-employee.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from 'src/app/entity/user';
+import { UserService } from 'src/app/services/user.service';
+import { EmployeesManagementComponent } from '../employees-management/employees-management.component';
 
 @Component({
   selector: 'app-add-employee',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent implements OnInit {
+  hide: boolean = true;
+  form: FormGroup;
+  currentUser: User;
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<AddEmployeeComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: EmployeesManagementComponent,
+    private fb: FormBuilder, 
+    private userService: UserService,
+    ) { }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user =>{
+      this.currentUser = user;
+      
+      console.log(this.currentUser)
+    })
+    this.form = this.fb.group({
+      username: ['', Validators.email],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+    });
   }
 
+  async onSubmit(){
+    await this.userService.registerEmployee({
+      username: this.form.value["username"],
+      first_name: this.form.value["first_name"],
+      last_name: this.form.value["last_name"],
+      password: this.form.value["password"],
+      // company_id: this.currentUser.company.company_id,
+      admin_id: this.currentUser.id
+    }
+    ).subscribe(x=>{
+      console.log(x)
+    })
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
