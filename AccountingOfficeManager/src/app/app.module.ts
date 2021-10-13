053260af1747f6da527fcb8393bef702fad48944
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-10-13 16:44:01 +0200

    Added MatMenuModule

--- AccountingOfficeManager/src/app/app.module.ts
+++ AccountingOfficeManager/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatSortModule } from '@angular/material/sort';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
@@ -95,7 +96,8 @@ const material = [
   MatChipsModule,
   MatAutocompleteModule,
   MatSortModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatMenuModule,
 ]
 
 @NgModule({
